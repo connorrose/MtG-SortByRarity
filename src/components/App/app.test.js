7adfc746aade58788ae525aa5317deade7f19b92
Connor Rose <connorrose.git@gmail.com>
2020-12-11 18:37:31 -0800

    restructure files/folders

--- src/components/App/app.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/jsx-filename-extension */
-import { render } from '@testing-library/react';
-import App from './app';
-
-test('Load and render page', () => {
-  const { getByText } = render(<App />);
-
-  expect(getByText(/example/i)).toBeVisible();
-});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import { render } from 'react-dom';
 import App from './components/App';
+import './styles.css';
 
 const mountNode = document.getElementById('root');
 render(<App />, mountNode);
