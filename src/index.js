d1dc54c0fc0261c03f28b932e5f849c16a509ee8
Connor Rose Delisle <connorrose.git@gmail.com>
2020-12-11 18:19:06 -0800

    Initialize updates

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable react/jsx-filename-extension */
+import { render } from 'react-dom';
+import App from './components/App';
+
+const mountNode = document.getElementById('root');
+render(<App />, mountNode);
